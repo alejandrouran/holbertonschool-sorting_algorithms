040b911a4678beca2429b079c86184ce372b6fe1
alejuran <alejandrourn@gmail>
2022-06-21 17:51:06 -0500

    buble commit

---
+++ 0-bubble_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - integers in ascending order using
+ * @array: print the array
+ * @size: array size
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t x, y;
+	int i;
+
+	if (size < 2)
+			return;
+	for (y = 0; y < size - 1; y++)
+	{
+		for (x = 0; x < size - y - 1; x++)
+		{
+			if (array[x] > array[x + 1])
+			{
+				i = array[x];
+				array[x] = array[x + 1];
+				array[x + 1] = i;
+				print_array(array, size);
+			}
+		}
+	}
+}
